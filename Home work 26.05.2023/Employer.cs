189bf5e6072776bfa03ca2eb370ae9beebc15198
Алекс <Alexandr_enid@mail.ru>
2023-09-21 20:14:23 +0500

    Добавьте файлы проекта.

---
+++ Home work 26.05.2023/Employer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_work_26._05._2023
+{
+    public class Employer
+    {
+        private DateTime birthDate;
+        private string name;
+        private string surname;
+        private string job_title;
+
+    
+        public DateTime BirthDate { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Job_title { get; set; }
+        public Employer()
+        {
+           
+            
+        }
+
+        
+
+     
+        public virtual void PrintWorkTime()
+        {
+            Console.WriteLine($"Сотрудник {this.Name} " +
+                $" не работает, потому что он экземпляр базового класса");
+        }
+        public virtual int GetAge(DateTime BirthDate)//метод вычисления сколько лет сотруднику
+        {
+            DateTime n = DateTime.Now; 
+            int age = n.Year - BirthDate.Year;
+
+            if (n.Month < BirthDate.Month || (n.Month == BirthDate.Month && n.Day < BirthDate.Day))
+                age--;
+
+            return age;
+        }
+
+    }
+}
+    
+    
+
+
